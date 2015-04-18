f025b56cd53ae5f1cff50101a516cb33fb3fc1bc
Thomas <trescenzi@Thomass-MacBook-Pro.local>
2015-04-18 17:37:14 -0400

    accidently didn't remove the extra emptyboard

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,5 @@ export const X = 'X';
 export const O = 'O';
 export const EMPTY_ROW = [E,E,E]
 export const EMPTY_BOARD = [EMPTY_ROW,EMPTY_ROW,EMPTY_ROW]
-export const EMPTY_BOARD = [EMPTY_ROW,EMPTY_ROW,EMPTY_ROW];
 export const EMPTY_SUPER_ROW = [EMPTY_BOARD, EMPTY_BOARD, EMPTY_BOARD];
 export const EMPTY_SUPER_BOARD = [EMPTY_SUPER_ROW, EMPTY_SUPER_ROW, EMPTY_SUPER_ROW];
